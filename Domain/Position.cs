fae181c0ee5ddcfba0e4a74a9e98c5da9886abb3
Matthew Christian Mac <matthewmac32@gmail.com>
2026-07-27 20:45:52 -0400

    Display position in chess notation

--- Domain/Position.cs
+++ Domain/Position.cs
@@ -7,4 +7,12 @@ public readonly record struct Position(int File, int Rank)
         return File >= 0 && File < 8 &&
                Rank >= 0 && Rank < 8;
     }
+
+    public override string ToString()
+    {
+        char fileLetter = (char)('a' + File);
+        int rankNumber = Rank + 1;
+
+        return $"{fileLetter}{rankNumber}";
+    }
 }
