777c6cb357b3666c086932025323fdc08a30ccb1
Matthew Christian Mac <matthewmac32@gmail.com>
2026-07-28 20:21:52 -0400

    Add queen movement

---
+++ src/Pieces/Queen.cs
@@ -0,0 +1,28 @@
+using ChessGame.Models;
+
+namespace ChessGame.Pieces;
+
+public sealed class Queen : Piece
+{
+    public Queen (PieceColor color) : base(color)
+    {
+    }
+
+    public override bool IsValidMovement(Position from, Position to)
+    {
+        if (!from.IsValid || !to.IsValid || from == to)
+        {
+            return false;
+        }
+
+        int rankDifference = Math.Abs(to.Rank - from.Rank);
+        int fileDifference = Math.Abs(to.File - from.File);
+
+        bool isStraight = from.File == to.File ||
+                        from.Rank == to.Rank;
+
+        bool isDiagonal = fileDifference == rankDifference;
+
+        return isStraight || isDiagonal;
+    }   
+}
